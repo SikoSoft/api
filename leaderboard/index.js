e98334787e171f280d22e0353afdc172044ed96a
Aaron Wright <aaron@sikosoft.com>
2020-05-30 22:46:58 +0200

    leaderboard now returns top score & others in range of new score

--- leaderboard/index.js
+++ leaderboard/index.js
@@ -3,13 +3,9 @@ const perPage = 25;
 
 module.exports = async function (context, req) {
   await db.getConnection().then(async (connection) => {
+    const body =
+      typeof req.body === "string" ? parse(req.body) : req.body ? req.body : {};
     if (req.method === "POST") {
-      const body =
-        typeof req.body === "string"
-          ? parse(req.body)
-          : req.body
-          ? req.body
-          : {};
       body.time = Math.floor(new Date().getTime() / 1000);
       body.ip =
         req.headers && req.headers["x-forwarded-for"]
@@ -21,15 +17,34 @@ module.exports = async function (context, req) {
       );
     }
     await connection
-      .query(
-        "SELECT name, score FROM leaderboard ORDER BY score DESC LIMIT ?",
-        [perPage]
-      )
+      .query("SELECT * FROM leaderboard ORDER BY score DESC")
       .then((rows) => {
-        jsonReply(
-          context,
-          rows.map((row, i) => ({ ...row, rank: i + 1 }))
-        );
+        const allRaw = rows.map((row, i) => ({ ...row, rank: i + 1 }));
+        const rank =
+          body.score && body.name
+            ? allRaw.filter(
+                (row) =>
+                  body.name === row.name &&
+                  body.score === row.score &&
+                  body.duration === row.duration &&
+                  body.time === row.time &&
+                  body.ip === row.ip
+              )[0].rank
+            : false;
+        const all = allRaw.map((row) => ({
+          name: row.name,
+          score: row.score,
+          rank: row.rank,
+        }));
+        let startOffset = rank - Math.ceil(perPage / 2);
+        if (startOffset < 1) {
+          startOffset = 1;
+        }
+        const records = [
+          all[0],
+          ...all.slice(startOffset, startOffset + perPage - 1),
+        ];
+        jsonReply(context, { rank, records });
       });
   });
 };
